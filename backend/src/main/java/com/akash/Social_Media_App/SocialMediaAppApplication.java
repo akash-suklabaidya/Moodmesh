fbf78922790e5a78d42502e1b5103fef1702b425
Akash Suklabaidya <akashsuklabaidya593@gmail.com>
2024-07-23 22:54:22 +0530

    Docker file added

--- backend/src/main/java/com/akash/Social_Media_App/SocialMediaAppApplication.java
+++ backend/src/main/java/com/akash/Social_Media_App/SocialMediaAppApplication.java
@@ -1,9 +1,12 @@
 package com.akash.Social_Media_App;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude =  {MongoAutoConfiguration.class})
 public class SocialMediaAppApplication {
 
 	public static void main(String[] args) {
